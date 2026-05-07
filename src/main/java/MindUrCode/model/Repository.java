a4cb9cb605f2cfa114f20b38e451da6e340113ec
joseph <josem.c7@icloud.com>
2026-05-07 07:42:10 -0400

    reposotory created

---
+++ src/main/java/MindUrCode/model/Repository.java
@@ -0,0 +1,99 @@
+package MindUrCode.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "repositories")
+public class Repository {
+
+@Id
+@GeneratedValue(strategy = GenerationType.UUID)
+private UUID id;
+
+@Column(name ="user_id", nullable = false)
+private UUID userId;
+
+@Column(nullable = false)
+private String name;
+
+@Column(name = "source_type")
+private String sourceType;
+
+@Column(name = "source_path")
+private String sourcePath;
+
+@Column(name = "added_at")
+private LocalDateTime addedAt;
+
+
+
+public Repository() {
+
+}
+
+
+
+public Repository(UUID userId, String name, String sourceType, String sourcePath) {
+    this.userId = userId;
+    this.name = name;
+    this.sourceType = sourceType;
+    this.sourcePath = sourcePath;
+    this.addedAt = LocalDateTime.now();
+}
+
+
+
+public UUID getId() {
+    return id;
+}
+
+public void getId(UUID id) {
+    this.id = id;
+}
+
+
+public UUID getUserId() {
+    return userId;
+}
+
+public void setUserId(UUID userId) {
+    this.userId = userId;
+}
+
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getSourceType() {
+    return sourceType;
+}
+public void setSourceType(String sourceType) {
+    this.sourceType = sourceType;
+}
+public String getSourcePath() {   
+    return sourcePath;
+}
+
+public void setSourcePath(String sourcePath) {
+    this.sourcePath = sourcePath;
+}
+public LocalDateTime getAddedAt() {
+    return addedAt;
+}
+public void setAddedAt(LocalDateTime addedAt) {
+    this.addedAt = addedAt;
+}
+
+
+
+}
