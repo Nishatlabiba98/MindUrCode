9fc396983a07120794c1b6f1fc94d02cc89b55cd
james <jamesstrine@gmail.com>
2026-05-07 08:39:07 -0400

    Whitespace

--- src/main/java/MindUrCode/model/Repository.java
+++ src/main/java/MindUrCode/model/Repository.java
@@ -1,13 +1,14 @@
 package MindUrCode.model;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Entity
 @Table(name = "repositories")
@@ -93,7 +94,4 @@ public LocalDateTime getAddedAt() {
 public void setAddedAt(LocalDateTime addedAt) {
     this.addedAt = addedAt;
 }
-
-
-
 }
