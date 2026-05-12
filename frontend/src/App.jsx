9f42f639b02bb85c0e408111f471349a84964aea
james <jamesstrine@gmail.com>
2026-05-12 15:00:02 -0400

    New Logo and Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './ThemeContext';
-import LandingPage from './pages/LandingPage';
+import Landing from './pages/Landing';
+import Dashboard from './pages/Dashboard';
 import SubmitRepo from './pages/SubmitRepo';
 import SimplificationEngine from './tools/SimplificationEngine';
 import CoverageAnalyzer from './tools/CoverageAnalyzer';
@@ -14,14 +15,15 @@ export default function App() {
     <ThemeProvider>
     <BrowserRouter>
       <Routes>
-        <Route path="/"         element={<LandingPage />} />
-        <Route path="/repos"    element={<SubmitRepo />} />
-        <Route path="/coverage" element={<CoverageAnalyzer />} />
-        <Route path="/clarity"  element={<ClarityScanner />} />
-        <Route path="/docs"     element={<DocumentationAssistant />} />
-        <Route path="/refactor" element={<RefactoringAdvisor />} />
-        <Route path="/simplify" element={<SimplificationEngine />} />
-        <Route path="*"         element={<LandingPage />} />
+        <Route path="/"          element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/repos"     element={<SubmitRepo />} />
+        <Route path="/coverage"  element={<CoverageAnalyzer />} />
+        <Route path="/clarity"   element={<ClarityScanner />} />
+        <Route path="/docs"      element={<DocumentationAssistant />} />
+        <Route path="/refactor"  element={<RefactoringAdvisor />} />
+        <Route path="/simplify"  element={<SimplificationEngine />} />
+        <Route path="*"          element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
     </ThemeProvider>
